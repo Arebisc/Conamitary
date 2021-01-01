c3c57120fb865cb4ec9f17eaa497b60d47185f92
Arebis <michal.sibera@windowslive.com>
2021-01-01 15:15:02 +0100

    Added database project

---
+++ Conamitary.Database/Receipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conamitary.Database
+{
+    public class Receipe
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Instructions { get; set; }
+        public string Ingredients { get; set; }
+    }
+}
