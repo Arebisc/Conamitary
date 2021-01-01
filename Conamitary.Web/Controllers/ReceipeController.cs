b5f8b6580bd105b118d5cae3a24acf258d443649
Arebis <michal.sibera@windowslive.com>
2021-01-01 15:07:05 +0100

    Removeed WeatherForecast and added ReceipeController

---
+++ Conamitary.Web/Controllers/ReceipeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conamitary.Web.Controllers
+{
+    [Route("api/receipe")]
+    public class ReceipeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
--- Conamitary.Web/Startup.cs
+++ Conamitary.Web/Startup.cs
@@ -19,7 +19,7 @@ namespace Conamitary.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllers();
             // In production, the Vue files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
