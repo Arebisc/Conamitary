b515bb3c7f67d9ffc1412e957514aef1527c3973
Arebis <michal.sibera@windowslive.com>
2021-02-21 21:45:45 +0100

    Added files microservice

---
+++ Microservices/Conamitary.Microservices.FileApi/Controllers/FileController.cs
@@ -0,0 +1,18 @@
+﻿using Conamitary.Dtos.Files;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conamitary.Microservices.FileApi.Controllers
+{
+    [Route("api/file")]
+    public class FileController : ControllerBase
+    {
+        public Task SaveFiles([FromForm] SaveFilesDto files)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
