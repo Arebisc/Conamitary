d76381a11859df3dee4c32f446a5affffd1bc5e2
Arebis <michal.sibera@windowslive.com>
2021-10-16 18:38:33 +0200

    Added appsettings.json configuration

--- Conamitary.FileConverter/Program.cs
+++ Conamitary.FileConverter/Program.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Conamitary.FileConverter
 {
     class Program
     {
+        public static IConfiguration configuration;
+
         static async Task Main(string[] args)
         {
             await new HostBuilder()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<MqListener>();
-                })
+                .ConfigureServices(ConfigureServices)
                 .RunConsoleAsync();
+        }
+
+        static void ConfigureServices(HostBuilderContext hostBuilderContext, 
+            IServiceCollection services)
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            services.AddSingleton(configuration);
+
+            services.AddHostedService<MqListener>();
         }
     }
 }
