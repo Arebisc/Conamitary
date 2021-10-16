4cae299286cec5875ed8500c3eef639c3904ec92
Arebis <michal.sibera@windowslive.com>
2021-10-16 18:29:54 +0200

    Added FileConverter project

---
+++ Conamitary.FileConverter/MqListener.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Conamitary.FileConverter
+{
+    public class MqListener : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Conamitary.FileConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Conamitary.FileConverter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MqListener>();
+                })
+                .RunConsoleAsync();
+        }
+    }
+}
